c25c9a541dd9c7cc8fc2e8be0c58a42148879395
queenwuli <942534046@qq.com>
2016-11-16 19:11:08 +0800

    页面初始化

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET user listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.get('/reg', function(req, res, next) {
+  res.render('user/reg')
+});
+router.get('/login', function(req, res, next) {
+  res.render('user/login')
+});
 module.exports = router;
